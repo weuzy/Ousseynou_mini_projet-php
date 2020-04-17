a8b7d296ba8d72c35e6abed4f7eb9b860cc645b8
weuzy <63518830+weuzy@users.noreply.github.com>
2020-04-17 04:43:33 -0100

    Add files via upload

---
+++ Joueur.php
@@ -0,0 +1,22 @@
+ 
+ 
+      <div class="middle">
+          <p>liste des joueurs par score</p>
+    <div class="gamerscore"></div>
+    
+    <div><button type="submit" class="suite" name="btn_submit" id="" value="">
+        <a href="" class="dec">Suivant</a></button></div>  
+
+    </div>        
+     
+          
+          
+
+
+              
+              
+         
+
+
+
+ 
