f83a3b538d3bd7d8c44edb1d230d212044ae6a99
weuzy <63518830+weuzy@users.noreply.github.com>
2020-04-17 04:37:17 -0100

    Update admin.php

--- Mon_mini_projet_Quizz/pages/admin.php
+++ Mon_mini_projet_Quizz/pages/admin.php
@@ -1,13 +1,61 @@
-Admin
+
 <?php
 is_connect();
 
-     echo $_SESSION['user']['prenom'];
-     echo $_SESSION['user']['nom'];
-     echo $_SESSION['user']['login'];
-     echo $_SESSION['user']['profil'];
-     echo $_SESSION['user']['photo'];
+     echo @$_SESSION['user']['prenom'];
+     echo @$_SESSION['user']['nom'];
+     echo @$_SESSION['user']['login'];
+     echo @$_SESSION['user']['profil'];
+     echo @$_SESSION['user']['photo'];
 
 ?>
-<br>
-<a href="index.php?statut=logout">Deconnexion</a>
+
+
+
+<div class="wrapper">
+    <div class="wrapper-header">
+        <div class="up-title">créer et paramètrer vos quizz</div>
+        <div>
+        <div><button type="submit" class="btn-position deconnexion" name="btn_submit" id="" value=""><a href="index.php?statut=logout" class="dec">Deconnexion</a></button></div>
+        </div>
+    </div>
+    <div class="wrapper-body">
+      <div class="create">
+          <div class="create-header">
+              <div class="picture-form">
+                  <div class="picture"></div>
+              </div>
+          </div>
+          <div class="list">
+              <nav class="list-form">
+                <ul>
+                <div class="liste-icon-form l1"></div>
+                <li><a href="">Liste Questions</a></li>
+                <div class="liste-icon-form l2"></div>
+                <li><a href="index.php?lien=accueil&page=inscription">Créer Admin</a></li>
+                <div class="liste-icon-form l3"></div>
+                <li><a href="index.php?lien=accueil&page=joueur">Liste Joueurs</a></li>
+                <div class="liste-icon-form l4"></div>
+                <li><a href="">Créer Questions</a></li>  
+                </ul>
+              </nav> 
+         </div>
+      </div>
+      
+ </div>   
+</div>
+
+<?php
+
+if ($_GET['page'] == 'joueur') 
+    {
+        include("Joueur.php");
+    }
+elseif ($_GET['page'] == 'inscription') 
+    {
+        include("Inscription.php");
+    }    
+
+
+
+?>
