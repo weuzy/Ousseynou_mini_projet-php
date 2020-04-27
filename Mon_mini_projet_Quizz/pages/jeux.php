aee7e51a3c7065ed7e8e5598c5f8c7e331a19865
weuzy <63518830+weuzy@users.noreply.github.com>
2020-04-27 07:36:49 -0100

    Update jeux.php

--- Mon_mini_projet_Quizz/pages/jeux.php
+++ Mon_mini_projet_Quizz/pages/jeux.php
@@ -1,58 +1,97 @@
-Joueur
+
 <?php
 is_connect();
+$connectedUser = getData()[$_SESSION['id']];
 
-     echo @$_SESSION['user']['prenom'];
-     echo @$_SESSION['user']['nom'];
-     echo @$_SESSION['user']['login'];
-     echo @$_SESSION['user']['profil'];
-     echo @$_SESSION['user']['photo'];
+$questionNumber = $question = $checkbox = [];
+define("NBRVALPARPAGE",1);
+$totalval = count($questionNumber);
+$nPage = ceil( $totalval/NBRVALPARPAGE);
 
+if (!isset($_GET['questionNumber'])) 
+{
+    $pageActu = 1;
+}
+else 
+{
+    $pageActu = $_GET['questionNumber'];
+    if($pageActu>=$nPage){
+        $pageActu=$nPage;
+    }elseif($pageActu<=1){
+        $pageActu=1;
+    }
+}
+ $iD = ( $pageActu - 1) * NBRVALPARPAGE;
+ $iF = $iD + NBRVALPARPAGE;
 ?>
-<br>
-<a href="index.php?statut=logout">Deconnexion</a>
+<style>
+ #navigation
+     {
+        position: relative; 
+        width: 100%;
+        display:flex;
+        top: 11.7rem;
+        justify-content: space-around
+    }
+    input
+    {
+        position: relative;
+        width: 2rem;
+        height: 2rem;
+        margin-left: 8rem;
+        top: 6.2rem;
+    }
+    label
+    {
+        position: relative;
+        top: 6rem;
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 2rem 
+    }
+   /* .nav1
+   {
+       left: 3rem
+   }  */
+</style>
 
 
-<div class="on">
-    <div class="on-header">
-        <div class="onside">s'inscrire</div>
-        <b>Pour tester votre niveau de culture générale</b>
-        <div class="file"></div>
-        <div class="img-form1">
-            <div class="img1"></div>
-            <h5> Avatar du Joueur</h5>
+<div class="wrapper">
+    <div class="wrapper-header">
+        <img src="<?php echo $connectedUser['photo'];?>" class="picture-form" style="float:left; width:66px; height:66px; border-color:#fff">
+        <div class="up-title" style="padding: 20px; font-size: 23px;"><?php echo $texte;?></div>
+        <div>
+        <div><a href="index.php?statut=logout" class="btn-position deconnexion" style=" top: -85px;">Deconnexion</a></div>
         </div>
-    </div>
-    <div class="on-body">
-            <form action="" method="POST" id="">
-            <div class="on-form">
-              <div class="direct1"> Prénom</div>
-              <input type="text" class="on-control" name="Prenom" id="" > 
-            </div>
-            <div class="on-form">
-            <div class="direct1"> Nom</div>
-              <input type="text" class="on-control" name="Nom" id="" > 
-            </div>   
-            <div class="on-form">
-            <div class="direct1"> Login</div>
-              <input type="text" class="on-control" name="Login" id="" > 
-            </div>   
-            <div class="on-form">
-            <div class="direct1"> Password</div>
-              <input type="password" class="on-control" name="Password" id="" > 
-            </div>   
-            <div class="on-form">
-            <div class="direct1"> Confirmer Password</div>
-              <input type="password" class="on-control" name="Confirmer Password" id="" > 
-            </div> 
-            <h5>Avatar</h5>
-            <div><button type="submit" class="fich1" name="btn_submit" id="" value="">
-                 <a href="" class="dec">Choisir un fichier</a></button></div> 
-            <div><button type="submit" class="compte1" name="btn_submit" id="" value="">
-                 <a href="" class="dec">Créer compte</a></button></div>     
-            </form>
-    </div>
+        </div>
+     <div class="wrapper-body">
+      <div class="interface">
+          <div class="ask">
+              <div class="ask-side"><h4>Question<?php echo $questionNumber;?>:</h4><br><p></p></div>
+              <div class="ask-value">4pts</div>
+          
+              <input type="checkbox" id="" name="">
+              <label for=""></label>
+          <br>
+          
+              <input type="checkbox" id="" name="">
+              <label for=""></label>
+          <br>
+         
+          
+              <input type="checkbox" id="ask" name="">
+              <label for="ask"></label>
+         
+          <div id="navigation">
+              <button type="submit" class="nav" name="btn_submit" id="" value=""><a href="index.php?lien=jeux" class="dec">Précédent</a></button>
+              <button type="submit" class="nav" name="btn_submit" id="" value=""><a href="index.php?lien=jeux" class="dec">Suivant</a></button>
+          </div> 
+          <div class="under"><div class="score"></div></div> 
+     </div>
+   </div>
+    </div>   
 </div>
+
 
 
 
