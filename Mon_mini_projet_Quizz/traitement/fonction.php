49550f2b5f0b5ceaaa4497897526509c75a9d95a
weuzy <63518830+weuzy@users.noreply.github.com>
2020-04-27 07:44:21 -0100

    Update fonction.php

--- Mon_mini_projet_Quizz/traitement/fonction.php
+++ Mon_mini_projet_Quizz/traitement/fonction.php
@@ -1,12 +1,11 @@
 <?php
 
 function connexion($login,$pwd){
-    $user = getData();
+    $users = getData();// getData c'est pour recupérer tous les éléments du fichiers Json
     foreach ($users as $key => $users) {
               if($users["login"] === $login && $users["password"] === $pwd){
-                  $_SESSION['users'] = $users;
-                  $_SESSION['statut'] = "login";
-                  if ($user["profil"] === "admin") {
+                $_SESSION['id'] = $key; // $_SESSION['id'] c'est la position de l'utilisateur dans le fichier json
+                  if ($users["profil"] === "admin") {
                       return "accueil";
                   }else {
                       return "jeux";
@@ -16,20 +15,18 @@ function connexion($login,$pwd){
     return "error";
 }
 function is_connect(){
-    if (!isset( $_SESSION['statut'])) {
+
+
+    if (!isset($_SESSION['admin']) && !isset($_SESSION['joueur'])) {
         header("location:index.php");
     }
 }
 
 function deconnexion(){
-        unset( $_SESSION['users']);
-        unset( $_SESSION['statut']);
         session_destroy();
 }
 
-function getData($file = "utilisateur"){
-            $data = file_get_contents("./data/".$file.".json");
-            $data = json_decode($data,true);
-            return $data;
+function getData($filename = "utilisateur"){
+            return json_decode(file_get_contents("./data/".$filename.".json"),true);
 }
 ?>
