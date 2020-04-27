b70737b77efc48c1dae5ec8c3dae464123110898
weuzy <63518830+weuzy@users.noreply.github.com>
2020-04-27 07:46:58 -0100

    Update index.php

--- Mon_mini_projet_Quizz/index.php
+++ Mon_mini_projet_Quizz/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>MON_MINI_PROJET_QUIZZ</title>
     <link rel="stylesheet" href="./public/css/quizz.css">
 </head>
 <body>
@@ -19,13 +19,34 @@
 
 
             if(isset($_GET['lien'])){
+                // *accueil* est le lien de la page Admin
+                // *jeux* est le lien de la page où l'utilisateur va jouer 
+                // *inscription* est le lien de la page où l'utilisateur va s'inscrire pour jouer
                 switch($_GET['lien']){
                     case "accueil":
+                        // titre principal de la page Admin
+                        $texte = "créer et paramètrer vos quizz";
+                        // titre de la partie inscrire pour la création compte Admin
+                        $title = "Pour proposer vos quizz";
+                        $nomAvatar = "Avatar Admin";
+                        // style de la partie création compte Admin de la page admin.php
+                        $top_in = "6%";
+                        $padding_in = "20px";
                           require_once("./pages/admin.php");  
                     break;
                     case "jeux":
+                        // titre principal de la page jeux
+                        $texte = "BIENVENUE SUR LA PLATEFORME DE JEU DE QUIZZ<br><br>JOUER ET TESTER VOTRE NIVEAU DE CULTURE GENERALE";
                           require_once("./pages/jeux.php");
-                    break;      
+                    break;
+                    case "inscription":
+                        // titre de la partie inscrire pour la création compte Joueur
+                        $title = "Pour tester votre niveau de culture générale";
+                        $nomAvatar = "Avatar du joueur";
+                        // style de la partie création compte Joueur de la page Inscription.php
+                        $right_in = "-28%";
+                          require_once("./pages/Inscription.php");
+                    break; 
                 } 
             }else {
                 if (isset($_GET['statut']) && $_GET['statut']==="logout") {
