e7a2d06f560fafebdd0bd9caabdc6da4df376671
weuzy <63518830+weuzy@users.noreply.github.com>
2020-04-27 07:25:53 -0100

    Update admin.php
    AJOUT DES TACHES DE US6 ET US7

--- Mon_mini_projet_Quizz/pages/admin.php
+++ Mon_mini_projet_Quizz/pages/admin.php
@@ -1,44 +1,64 @@
 
 <?php
 is_connect();
+// getData c'est pour recupérer tous les éléments du fichiers Json
+// $_SESSION['id'] c'est la position de l'utilisateur dans le fichier json
+// $connectedUser c'est la table qui contient toutes les informations sur l'utilisateur connecté
 
-     echo @$_SESSION['user']['prenom'];
-     echo @$_SESSION['user']['nom'];
-     echo @$_SESSION['user']['login'];
-     echo @$_SESSION['user']['profil'];
-     echo @$_SESSION['user']['photo'];
-
+$connectedUser = getData()[$_SESSION['id']];
+$listQ = '';
+$creerA = '';
+$listJ = '';
+$creerQ = '';
+$_SESSION['listQ']= '';
+$_SESSION['creerA'] = '';
+$_SESSION['listJ']= '';
+$_SESSION['creerQ'] = '';
+// Récupération de la classe *-active* qui marque la différence 
+// de l'icone cliqué par rapport aux autres icones dans la partie du menu 
+    if ($_GET['page'] == 'inscription') 
+    {
+        $_SESSION['creerA'] ='-active';
+        $creerA = $_SESSION['creerA'];
+    }
+    elseif($_GET['page'] == 'joueur') 
+    {
+        $_SESSION['listJ'] ='-active';
+        $listJ = $_SESSION['listJ'];
+    } 
+    elseif($_GET['page'] == 'listquestion') 
+    {
+        $_SESSION['listQ'] ='-active';
+        $listQ = $_SESSION['listQ'];
+    } 
+    elseif($_GET['page'] == 'question') 
+    {
+        $_SESSION['creerQ'] ='-active';
+        $creerQ = $_SESSION['creerQ'];
+    }     
 ?>
 
-
-
+    <!-- Structuration de l'HTML de la page ADMIN -->
 <div class="wrapper">
     <div class="wrapper-header">
-        <div class="up-title">créer et paramètrer vos quizz</div>
+    <!-- $texte est le titre de la page qui est la seule différence au niveau de la partie header à celle de jeux.php-->
+        <div class="up-title"><?php echo $texte;?></div> 
         <div>
-        <div><button type="submit" class="btn-position deconnexion" name="btn_submit" id="" value=""><a href="index.php?statut=logout" class="dec">Deconnexion</a></button></div>
+        <div><a href="index.php?statut=logout" class="btn-position deconnexion">Deconnexion</a></div>
+        
         </div>
-    </div>
     <div class="wrapper-body">
       <div class="create">
           <div class="create-header">
-              <div class="picture-form">
-                  <div class="picture"></div>
-              </div>
+              <img src="<?= $connectedUser['photo'] ?>" style="position:relative; left:0" class="picture-form">
           </div>
-          <div class="list">
-              <nav class="list-form">
-                <ul>
-                <div class="liste-icon-form l1"></div>
-                <li><a href="">Liste Questions</a></li>
-                <div class="liste-icon-form l2"></div>
-                <li><a href="index.php?lien=accueil&page=inscription">Créer Admin</a></li>
-                <div class="liste-icon-form l3"></div>
-                <li><a href="index.php?lien=accueil&page=joueur">Liste Joueurs</a></li>
-                <div class="liste-icon-form l4"></div>
-                <li><a href="">Créer Questions</a></li>  
+          <div class="list" style="box-sizing: border-box;">
+                <ul style="box-sizing: border-box;">
+                    <li class="liste-icon-form <?= $listQ ?>" style="background-image: url('./public/icones/ic-liste<?= $listQ ?>.png');"><a href="">Liste Questions</a></li>
+                    <li class="liste-icon-form <?= $creerA ?>" style="background-image: url('./public/icones/ic-ajout<?= $creerA ?>.png');"><a href="index.php?lien=accueil&page=inscription">Créer Admin</a></li>
+                    <li class="liste-icon-form <?= $listJ ?>" style="background-image: url('./public/icones/ic-liste<?= $listJ ?>.png');"><a href="index.php?lien=accueil&page=joueur">Liste Joueurs</a></li>
+                    <li class="liste-icon-form <?= $creerQ ?>" style="background-image: url('./public/icones/ic-ajout<?= $creerQ ?>.png');"><a href="index.php?lien=accueil&page=question">Créer Questions</a></li>
                 </ul>
-              </nav> 
          </div>
       </div>
       
@@ -46,14 +66,19 @@ is_connect();
 </div>
 
 <?php
-
-if ($_GET['page'] == 'joueur') 
-    {
-        include("Joueur.php");
-    }
-elseif ($_GET['page'] == 'inscription') 
+    // inclusion des pages qui nous permet d'afficher une partie du menu 
+    // une fois cliquer sur le lien souhaiter dans la page Admin
+    if ($_GET['page'] == 'inscription') 
     {
         include("Inscription.php");
+    }
+elseif ($_GET['page'] == 'joueur') 
+    {
+        include("Joueur.php");
+    }    
+ elseif ($_GET['page'] == 'question') 
+    {
+        include("question.php");
     }    
 
 
